f0f1557421664fceda2bdbb8799336619b23d2ce
d <dudanger@gmail.com>
2026-05-15 11:46:12 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// 一个简单的函数
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+// 如果直接运行此文件，输出问候
+if (require.main === module) {
+    console.log(greet('World'));
+}
+
+module.exports = greet;
